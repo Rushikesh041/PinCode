2ee2017128004870438c1bf6c0391d638e7c7653
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-26 05:50:43 +0530

    UC1Valid

---
+++ PinCode/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PinCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter PINCODE : ");
+            long pin = Convert.ToInt64(Console.ReadLine());
+            RegexPattern.Pattern(pin);
+        }
+    }
+}
---
+++ PinCode/RegexPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PinCode
+{
+    public class RegexPattern
+    {
+        public static void Pattern(long pattern)
+        {
+            Regex code = new Regex("^[1-9]{1}[0-9]{5}$");
+            bool matches = code.IsMatch(Convert.ToString(pattern));
+
+            if (matches == true)
+            {
+                Console.WriteLine("Pincode is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Pincode");
+            }
+        }
+    }
+}
